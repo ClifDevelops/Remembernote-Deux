b982601cd2bb5eb5409428f2781884c773626597
ClifDevelops <ClifDevelops@gmail.com>
2021-06-22 17:17:45 -0600

    finished the backend setup

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,7 +3,13 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 
 router.use('/session', sessionRouter);
-
 router.use('/users', usersRouter);
+
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+});
+
+
 
 module.exports = router;
