13c0c9e334abb3ae1f837a9eb104cf81194847cc
ClifDevelops <ClifDevelops@gmail.com>
2021-06-23 17:46:00 -0600

    added a splash flow

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Splash.css";
+import Logo from '../../images/remembernote-logo.png'
 
 const Splash = () => {
 
     return (
         <div className='splash-container'>
-            <div className='splash-title'>Remembernote</div>
+            <div>
+                <img className='splash-logo' src={Logo} alt='logo'/>
+            </div>
+            <div className='splash-text'> Remembernote is a site designed to help you record your life one memory at a time! Tag and date your memories, add pictures if you please, and ultimately have an easily searchable, private journal of your life!</div>
+            <div className='splash-links-container'>
+                <NavLink className='splash-links' to='/login'>Login Here</NavLink>
+                <NavLink className='splash-links' to='/signup'>Signup Here</NavLink>
+            </div>
+            
+            
            
             <div className='splash-banner-1'></div>
 
