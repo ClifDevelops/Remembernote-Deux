0874b07b1487947449ff667cd313a39d7574441b
ClifDevelops <ClifDevelops@gmail.com>
2021-06-23 11:17:55 -0600

    created a bunch of login component and redux logix

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import sessionReducer from './session';
 
 const rootReducer = combineReducers({
+    session: sessionReducer,
 });
 
 let enhancer;
+
+
 
 if (process.env.NODE_ENV === 'production') {
   enhancer = applyMiddleware(thunk);
