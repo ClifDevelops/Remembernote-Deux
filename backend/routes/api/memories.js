5eaff82bd723c84a69cad0bde60c1ccb00438565
ClifDevelops <ClifDevelops@gmail.com>
2021-06-25 17:19:45 -0600

    memories are now posting

---
+++ backend/routes/api/memories.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+
+const { setTokenCookie, requireAuth, getCurrentUserId } = require("../../utils/auth");
+const { User, Memory, Tag, MemoryTag } = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+const router = express.Router();
+
+const validateMemory = [
+  check("title")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a title for your memory.")
+    .isLength({ min: 1, max: 100 })
+    .withMessage("Please provide a valid email or username."),
+  check("dateOfMemory")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a valid date in the format YYYY-MM-DD")
+    .isLength({ min: 10, max: 10 })
+    .withMessage("Please provide a valid date in the format YYYY-MM-DD"),
+  check("location")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a location.")
+    .isLength({ min: 1, max: 100 }),
+  check("memoryRating")
+    .exists({ checkFalsy: true })
+    .withMessage("Please select a memory rating."),
+  check("body")
+    .exists({ checkFalsy: true })
+    .withMessage("Please record even a snippet of a memory.")
+    .isLength({ min: 1 })
+    .withMessage("Please record even a snippet of a memory."),
+  handleValidationErrors,
+];
+
+
+router.get("/",
+    requireAuth,
+    asyncHandler(async function (req, res) {
+      const currentUserId = parseInt(req.cookies.user, 10);
+      const memories = await Memory.findAll({
+        where: {
+          userId: currentUserId,
+        },
+        order:[["dateOfMemory", "desc"]]
+      });
+      return res.json(memories);
+    }))
+
+
+
+router.post(
+    "/",
+    requireAuth,
+    validateMemory,
+    asyncHandler(async function (req, res) {
+      // console.log(req.cookies.user);
+      // const userId = parseInt(req.cookies.user, 10);
+      const {title, dateOfMemory, location, memoryRating, body, userId} = req.body;
+      const memory = await Memory.create({
+        title,
+        dateOfMemory,
+        location,
+        memoryRating,
+        body,
+        userId
+      });
+      res.json(memory);
+  
+    })
+  )
+
+
+module.exports = router;
---
+++ frontend/src/store/memories.js
@@ -0,0 +1,93 @@
+import { csrfFetch } from "./csrf";
+
+const SET_MEMORY = "memory/GET";
+const ADD_MEMORY = "memory/ADD";
+const DELETE_MEMORY = "memory/DELETE";
+const UNSET_MEMORIES = "memories/UNSET";
+const SET_MEMORIES = "memories/SET"
+
+
+//ACTIONS
+// const setMemory = (memory) => ({
+//   type: SET_MEMORY,
+//   payload: memory,
+// });
+
+const load = (memories) => ({
+    type: SET_MEMORIES,
+    payload: memories
+});
+
+const addMemory = (memory) => ({
+    type: ADD_MEMORY,
+    payload: memory,
+});
+
+const unset = () => {
+    return {
+        type: UNSET_MEMORIES
+    }
+}
+//THUNKS (Async Actions)
+
+
+export const setMemories = () => async dispatch => {
+    const response = await csrfFetch(`/api/memories`);
+    if (!response.ok) {
+      throw response;
+    }
+    const memories = await response.json();
+    dispatch(load(memories));
+}
+
+export const unsetMemories = () => async (dispatch) => {
+  
+  dispatch(unset());
+};
+
+export const createMemory = data => async dispatch => {
+    console.log(data);
+    const response = await csrfFetch(`/api/memories`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (response.ok) {
+        const memory = await response.json();
+        dispatch(addMemory(memory));
+        return memory;
+    }
+}
+
+
+
+
+const initialState = {};
+//REDUCERS
+const memoriesReducer = (state = initialState, action) => {
+    let newState = {};
+    switch(action.type) {
+        case SET_MEMORY:
+            return
+        case ADD_MEMORY:
+            state[action.payload.id] = action.payload;
+            return state;
+        case SET_MEMORIES:
+            action.payload.forEach((memory) => {
+                newState[memory.id] = memory;
+            });
+            return newState;
+        case UNSET_MEMORIES:
+            newState.memories = {};
+            return newState;
+
+        
+        default:
+            return state;
+    }
+}
+
+export default memoriesReducer;
